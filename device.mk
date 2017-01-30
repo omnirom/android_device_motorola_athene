2088cba7ede30ac76572bab5da88dd88b1bbbb83
Eino-Ville Talvala <etalvala@google.com>
2017-01-29 18:22:44 -0800

    athene: Disable camera treble path
    Test: Compiles, property in build.prop as expected
    Bug: 34786432
    Change-Id: Ia09fdaf3a38d07536b0f5ec97770169ce6d7f660

--- device.mk
+++ device.mk
@@ -125,6 +125,10 @@ PRODUCT_PACKAGES += \
     camera.device@3.2-impl \
     android.hardware.camera.provider@2.4-impl
 
+# Disable camera Treble path
+PRODUCT_PROPERTY_OVERRIDES += \
+    camera.disable_treble=true
+
 # Charger
 PRODUCT_PACKAGES += \
     omni_charger_res_images
