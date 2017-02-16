0c782edaed12a2833694ac9ba7e1be15628e2c90
Stefan Berger <s.berger81@gmail.com>
2017-02-16 01:01:59 +0100

    athene: Update PRODUCT_PACKAGES
    Add Exchange service to build

--- device.mk
+++ device.mk
@@ -180,9 +180,10 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml
 
-# Messaging (SMS/MMS)
+# Messaging (SMS/MMS/Email)
 PRODUCT_PACKAGES += \
-    messaging
+    messaging \
+    Exchange2
 
 # OMX
 PRODUCT_PACKAGES += \
