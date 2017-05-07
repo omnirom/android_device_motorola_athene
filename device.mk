f73c15729387ca961257970550ae58a973507e6a
Stefan Berger <s.berger81@gmail.com>
2017-05-07 13:16:46 +0200

    athene: Align audio effects with DSP manager
    Also make sure device config gets loaded first
    
    Change-Id: I1e9ecc9eb1187f9c0a0b7dca8be1fbada88e2372

--- device.mk
+++ device.mk
@@ -83,10 +83,9 @@ PRODUCT_PACKAGES += \
 # Audio
 PRODUCT_PACKAGES += \
     audio.primary.msm8952
-	
-	
+
 PRODUCT_COPY_FILES +=  \
-    $(LOCAL_PATH)/audio/audio_effects.conf:system/vendor/etc/audio_effects.conf \
+    $(LOCAL_PATH)/audio/audio_effects_vendor.conf:system/etc/audio_effects_vendor.conf \
     $(LOCAL_PATH)/audio/audio_platform_info.xml:system/etc/audio_platform_info.xml \
     $(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf \
     $(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths.xml \
