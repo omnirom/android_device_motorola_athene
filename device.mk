5edae18f151203ad572cd01bf14557b915effa91
Vachounet <vachounet@live.fr>
2017-08-23 13:07:31 +0200

    athene : Add HIDL manifest
    Change-Id: Ifc8d2115e034b9983e0fff69a14d71a978859b1e

--- device.mk
+++ device.mk
@@ -179,6 +179,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/gps/etc/sap.conf:system/etc/sap.conf \
     $(LOCAL_PATH)/gps/etc/xtwifi.conf:system/etc/xtwifi.conf
 
+# HIDL
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/manifest.xml:system/vendor/manifest.xml
+
 # IRQ
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/msm_irqbalance.conf:system/vendor/etc/msm_irqbalance.conf
