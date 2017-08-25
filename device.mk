0e0e286ba94adffa4ef67809f8c89a80143ee0dd
Vachounet <vachounet@live.fr>
2017-08-25 16:01:05 +0000

    athene : build USB HAL
    Change-Id: I721883fcfd961dfc21ff5ffe3e93d6225b06923a

--- device.mk
+++ device.mk
@@ -296,6 +296,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine-athene.conf:system/etc/thermal-engine-athene.conf
 
+# USB HAL
+PRODUCT_PACKAGES += \
+    android.hardware.usb@1.0-service
+
 # Vibrator
 PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-impl
