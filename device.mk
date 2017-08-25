5474b06015d9a298a04862221470309dfdaafcc9
Vachounet <vachounet@live.fr>
2017-08-25 14:39:07 +0000

    athene : update audio for O
    Change-Id: I356f168a1797f7cf3a2f7acede5ea269393d6a86

--- device.mk
+++ device.mk
@@ -67,19 +67,26 @@ PRODUCT_AAPT_CONFIG := normal
 
 # Audio
 PRODUCT_PACKAGES += \
-    audiod \
     audio.a2dp.default \
     audio.r_submix.default \
     audio.usb.default \
     libaudio-resampler \
-    libqcompostprocbundle \
+
+PRODUCT_PACKAGES += \
+
     libqcomvisualizer \
     libqcomvoiceprocessing \
     libqcomvoiceprocessingdescriptors \
+    libqcompostprocbundle \
     libtinyalsa \
     libtinycompress \
-    libtinyxml \
-    tinymix
+    libtinyxml
+
+PRODUCT_PACKAGES += \
+    android.hardware.audio@2.0-impl \
+    android.hardware.audio.effect@2.0-impl \
+    android.hardware.broadcastradio@1.0-impl \
+    android.hardware.soundtrigger@2.0-impl
 
 # Audio
 PRODUCT_PACKAGES += \
