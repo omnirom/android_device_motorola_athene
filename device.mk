dbdd4955869a86248cb5688c2e3397f977a47d9e
Nikit Sharma <nikitsharma92@gmail.com>
2017-09-09 02:33:27 +0530

    athene: Build sensors HAL
    Change-Id: Ia43f43d30486129d1274a88f720880469d43b675

--- device.mk
+++ device.mk
@@ -288,8 +288,8 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/sensors/sensor_def_qcomdev.conf:system/etc/sensors/sensor_def_qcomdev.conf \
     $(LOCAL_PATH)/sensors/hals.conf:system/etc/sensors/hals.conf
 
-#PRODUCT_PACKAGES += \
-#    android.hardware.sensors@1.0-impl
+PRODUCT_PACKAGES += \
+    android.hardware.sensors@1.0-impl
 
 # Stlport
 PRODUCT_PACKAGES += \
