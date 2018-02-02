27eb5e6b8889cd7aac51d81b2dfbe0f8dac7632f
Nikit Sharma <nikitsharma92@gmail.com>
2018-02-02 15:46:17 +0530

    athene: Update IMS shim
    Change-Id: Ica32f6d9dfa76b122f14bdd5f035fb66b3e70acc

--- libshims/ims/Android.mk
+++ libshims/ims/Android.mk
@@ -18,8 +18,8 @@ LOCAL_PATH := $(call my-dir)
 
 # IMS
 include $(CLEAR_VARS)
-LOCAL_SRC_FILES := MediaBuffer.c
-LOCAL_SHARED_LIBRARIES := libstagefright_foundation
+LOCAL_SRC_FILES := MediaBuffer.cpp
+LOCAL_SHARED_LIBRARIES  := libui libgui libstagefright_foundation
 LOCAL_MODULE := libshims_ims
 LOCAL_MODULE_TAGS := optional
 include $(BUILD_SHARED_LIBRARY)
