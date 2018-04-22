492c45d0744b28a6f531a9eb3ddf551ed4bfc12a
Stefan Berger <s.berger81@gmail.com>
2018-04-22 21:30:27 +0200

    athene: Enable widevine DRM
    Change-Id: Id6e154d823f84c2f522e4626ba16ce9f4daa90dd

--- device.mk
+++ device.mk
@@ -227,7 +227,8 @@ PRODUCT_PACKAGES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.0-impl
+    android.hardware.drm@1.0-impl \
+    android.hardware.drm@1.0-service
 
 # OEM
 PRODUCT_COPY_FILES += \
