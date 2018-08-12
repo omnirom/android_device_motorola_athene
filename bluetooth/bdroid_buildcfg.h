ea6351abde99032de2541d9e97e3f627a69d2bb5
dianlujitao <dianlujitao@lineageos.org>
2018-08-12 20:24:29 +0100

    athene: bt: Undef PROPERTY_VALUE_MAX after use
    - Fixes build of bluetooth stack

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -20,6 +20,8 @@
 #include <cutils/properties.h>
 #include <string.h>
 
+#undef PROPERTY_VALUE_MAX
+
 #define BTM_DEF_LOCAL_NAME "Moto G4"
 #define BTA_DISABLE_DELAY 1000 /* in milliseconds */
 #define BLUETOOTH_QTI_SW TRUE
