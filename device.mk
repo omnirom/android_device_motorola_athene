bbef348fcd11227ea8989376bf84cb2046c883d1
Stefan Berger <s.berger81@gmail.com>
2018-11-04 17:06:08 +0100

    athene: Move audio configs to vendor
    Change-Id: Iac90de6646197ad4727771d00e756bfef795ab89

--- device.mk
+++ device.mk
@@ -95,7 +95,7 @@ PRODUCT_PACKAGES += \
     libshims_speakerbundle
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/audio/audio_effects_vendor.conf:system/etc/audio_effects_vendor.conf \
+    $(LOCAL_PATH)/audio/audio_effects_vendor.conf:system/vendor/etc/audio_effects_vendor.conf \
     $(LOCAL_PATH)/audio/audio_platform_info.xml:system/etc/audio_platform_info.xml \
     $(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf \
     $(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths.xml \
